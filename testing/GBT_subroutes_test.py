542fbb92a9c87e3340609a67308eb8f9dc444f1d
mateonoel2 <mateo.noel@utec.edu.pe>
2023-07-03 17:24:36 -0500

    casi todo

---
+++ testing/GBT_subroutes_test.py
@@ -0,0 +1,65 @@
+from pyspark.sql import SparkSession
+from pyspark.ml.regression import GBTRegressionModel
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.feature import VectorAssembler, StandardScalerModel
+from pyspark.sql.functions import abs, asc
+import sys
+
+def handle_error(e):
+    print("Error occurred during model training: ", e)
+    sys.exit(1)
+
+if __name__ == '__main__':    
+    try:
+        spark = SparkSession.builder \
+        .config("spark.executor.instances", "4")\
+        .config("spark.executor.cores", "1")\
+        .config("spark.driver.extraJavaOptions", "-XX:-UseGCOverheadLimit") \
+        .config("spark.executor.extraJavaOptions", "-XX:-UseGCOverheadLimit") \
+        .getOrCreate()
+        
+        data = spark.read.parquet("../dataset").coalesce(1)\
+                .orderBy(asc("month"), asc("day_of_month"), asc("exit_time")).cache()
+        
+        data = data.select("day_of_week", "exit_time", "distance", "exit_stop", "target_stop", "label")
+
+        data.show(5)
+    
+        assembler = VectorAssembler(inputCols=data.columns[:-1], outputCol="features_a")
+        data = assembler.transform(data).select("features_a", "label") 
+
+        scaler = StandardScalerModel.load("../scalers/spark_subroutes")
+        data = scaler.transform(data).select("features", "label")
+
+        split_point = int(data.count() * 0.8)
+
+        train_data = data.limit(split_point).cache() 
+        test_data = data.subtract(train_data).cache()  
+
+        model = GBTRegressionModel.load("../models/gbt_subroutes")
+
+        predictions = model.transform(test_data)
+
+        predictions = predictions.withColumn("abs_diff", abs(predictions["label"] - predictions["prediction"]))
+
+        total_count = predictions.count()
+
+        ranges = [600, 300, 60, 30]
+
+        for r in ranges:
+            within_range_count = predictions.filter(predictions["abs_diff"] <= r).count()
+            percent_within_range = within_range_count / total_count * 100
+            print("{:.2f}% of data is within a range of {:.0f} seconds".format(percent_within_range, r))
+
+        evaluator = RegressionEvaluator(labelCol="label", predictionCol="prediction", metricName="rmse")
+        rmse = evaluator.evaluate(predictions)
+        print("Root Mean Squared Error (RMSE) = {:.4f}".format(rmse))
+
+        evaluator = RegressionEvaluator(labelCol="label", predictionCol="prediction", metricName="r2")
+        r2 = evaluator.evaluate(predictions)
+        print("R-squared (R2) = {:.4f}\n".format(r2))
+
+    except Exception as e:
+        handle_error(e)
+    finally:
+        spark.stop()
--- testing/RNN_subroutes_test.py
+++ testing/RNN_subroutes_test.py
@@ -11,7 +11,6 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 from keras.models import load_model
 
-# Define a function to handle errors during model training
 def handle_error(e):
     print("Error occurred during model training: ", e)
     sys.exit(1)
@@ -30,16 +29,17 @@ if __name__ == '__main__':
 
         labels = data['label'].values
         
-        # Split the data into training and testing sets
         X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, shuffle=False)
 
-        model = load_model('../models/ANN_short_135.h5')
+        X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
+        X_test = np.reshape(X_test, (X_test.shape[0], 1, X_test.shape[1]))
+
+        model = load_model('../models/RNN_short_27.h5')
      
         y_pred = model.predict(X_test)
         r2 = r2_score(y_test, y_pred)
         rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 
-        # Define the time thresholds in the range
         ranges = [600, 300, 60, 30]
         
         y_pred = y_pred.flatten()
@@ -51,27 +51,8 @@ if __name__ == '__main__':
             percent_within_range = np.sum(within_range) / y_pred.shape[0] * 100
             print("{:.2f}% of data is within a range of {:.0f} seconds".format(percent_within_range, r))
 
-        # Print metrics
         print("R-squared: {:.4f}".format(r2))
         print("RMSE: {:.4f}".format(rmse))
-
-        #unscale x:
-        X_test = scaler.inverse_transform(X_test)
-
-        # Plotting predicted time
-        plt.scatter(X_test[:, 1], y_pred, c='b', label='Predicted Time', s=10, alpha=0.5)
-
-        # Plotting actual time
-        plt.scatter(X_test[:, 1], y_test, c='r', label='Actual Time', s=1, alpha=0.5)
-
-        plt.xlabel('Exit Time')
-        plt.ylabel('Arrive Time')
-        plt.legend()
-        plt.title('Exot time vs Predicted and Actual Arrive Time')
-
-        # Showing only one dot per plot
-        plt.legend(scatterpoints=1, frameon=False, labelspacing=1)
-        plt.show()
 
     except Exception as e:
         handle_error(e)
